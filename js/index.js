765412928badf5bd0dbc60d15cd7feb9650161bf
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-13 11:19:08 +0100

    improved js function

--- js/index.js
+++ js/index.js
@@ -1,21 +1,12 @@
 var song = document.getElementById('mySong');
 song.volume = 0.1;
 
-function toggleItem(button) {
-  for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener('click', function (e) {
-      var current = this;
-      for (var i = 0; i < button.length; i++) {
-        if (current != button[i]) {
-          button[i].classList.remove('active');
-        } else if (current.classList.contains('active') === true) {
-          current.classList.remove('active');
-        } else {
-          current.classList.add('active');
-        }
-      }
-      e.preventDefault();
-    });
-  }
-}
-toggleItem(document.querySelectorAll('.footer-button'));
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+
+play.addEventListener('click', () => {
+  pause.classList.add('active');
+});
+pause.addEventListener('click', () => {
+  pause.classList.remove('active');
+});
