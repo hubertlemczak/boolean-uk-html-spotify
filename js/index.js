08ce30b67002f40e689b64f51827b9ce60cd768d
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-13 11:31:56 +0100

    added play/pause events into js file

--- js/index.js
+++ js/index.js
@@ -3,10 +3,13 @@ song.volume = 0.1;
 
 const play = document.getElementById('play');
 const pause = document.getElementById('pause');
+const audio = document.getElementById('mySong');
 
 play.addEventListener('click', () => {
   pause.classList.add('active');
+  audio.play();
 });
 pause.addEventListener('click', () => {
   pause.classList.remove('active');
+  audio.pause();
 });
